1e6349f2da29f8333e10f37896fb7c915af0074b
Jeff Wang <wa.jeff@outlook.com>
2025-05-16 13:51:35 -0400

    Fixed relative pathing to absolute in server.js for monogodb uri

--- server/server.js
+++ server/server.js
@@ -1,42 +1,43 @@
 import express from 'express';
-import {MongoClient} from 'mongodb'
+import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
-import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
 
-dotenv.config({ path: path.resolve(process.cwd(), '../.env') });
-console.log('Current working directory:', process.cwd());
-console.log('Resolved path to .env:', path.resolve(process.cwd(), '../.env'));
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
+dotenv.config({ path: resolve(__dirname, '../.env') });
 
 const app = express();
 const port = 3001;
 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
 
-const uri = process.env.MONGODB_URI
-console.log('MONGODB_URI:', process.env.MONGODB_URI);
+const uri = process.env.MONGODB_URI;
+console.log('MONGODB_URI:', uri); 
+
 let db;
 
 async function startServer() {
-  try{
+  try {
     const client = await MongoClient.connect(uri);
     console.log('Connected to MongoDB Atlas');
-
     db = client.db('personal');
 
     app.get('/', (req, res) => {
       res.send('Hello from Express!');
     });
+
     app.listen(port, () => {
       console.log(`Server running on http://localhost:${port}`);
     });
   } catch (err) {
-  console.error('MongoDB connection failed:', err);
-}
-
+    console.error('MongoDB connection failed:', err.message);
+  }
 }
 
 startServer();
-
-
