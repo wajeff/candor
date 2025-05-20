a955918e44ea26fbde1e0a31974886cd9390ad95
Jeff Wang <wa.jeff@outlook.com>
2025-05-20 06:43:58 -0400

    Installed react-pdf Added react-pdf boilerplate

--- client/src/Pages/ContractEditor.jsx
+++ client/src/Pages/ContractEditor.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
 import Nav from '../Components/Nav/Nav'
 import Tiptap from '../Components/TipTap/TipTap'
+import PDFRenderer from '../Components/PDFRenderer/PDFRenderer'
 
 const ContractEditor = () => {
   return (
     <div>
       <Nav/>
       <Tiptap/>
+      <PDFRenderer/>
     </div>
   );
 }
